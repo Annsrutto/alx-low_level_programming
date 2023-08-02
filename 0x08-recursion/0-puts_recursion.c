ae22a51713d5e3e8280ad422030978c0121f3629
Annsrutto <annsrutto567@gmail.com>
2023-08-02 16:04:03 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,16 +4,17 @@
  * _puts_recursion - prints a string
  * @s: input value
  *
+ * Return: Always 0 (Success)
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-	}
-	return;
-
 	_putchar(*s);
 	_puts_recursion(s + 1);
+	}
+	else
+	_putchar('\n');
+
 }
