6ca5fc7c8771be8c80b1b61e01ad120fc4ea816f
Annsrutto <annsrutto567@gmail.com>
2023-08-10 02:53:54 +0300

    0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
-	s = malloc(sizeof(char) * size);
+	s = (char *) malloc(sizeof(char) * size);
 	if (s == 0 || s == NULL)
 	{
 		return (NULL);
