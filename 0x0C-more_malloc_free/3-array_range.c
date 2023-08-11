0dff5f30d61ac6c7de8ef0d9b8c81a67f1819e02
Annsrutto <annsrutto567@gmail.com>
2023-08-12 01:39:19 +0300

    101-mul.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,18 +1,18 @@
-nclude <stdlib.h>
+#include <stdlib.h>
 #include "main.h"
 /**
-    * array_range - creates integer array
-      * @min: first number and start of range
-        * @max: end of range
-	  * Return:pointer to array
-	    */
+* array_range - creates integer array
+* @min: first number and start of range
+* @max: end of range
+* Return:pointer to array
+ */
 int *array_range(int min, int max)
 {
-		int *p;
-			int i;
+	int *p;
+	int i;
 
-				if (min > max)
-						{
+	if (min > max)
+	{
 									return (NULL);
 										}
 					p = malloc(sizeof(int) * (max - min + 1));
