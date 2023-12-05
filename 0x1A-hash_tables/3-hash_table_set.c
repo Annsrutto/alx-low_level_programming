3579aaaaca771e8b15e90e5507c5c789d87e5059
Annsrutto <annsrutto567@gmail.com>
2023-12-05 05:11:18 +0300

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,13 +9,27 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node, *current_node;
+	hash_node_t *node, *new_node;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
+	/* Check if the key already exists and update value if it does*/
+	for (node = ht->array[index]; node != NULL; node = node->next)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);  /*Free the old value*/
+			node->value = strdup(value);  /* Duplicate and set the new value*/
+			if (node->value == NULL)
+				return (0);
+			return (1);
+		}
+	}
+
+	/* Key does not exist, create a new node*/
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
@@ -24,15 +38,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = strdup(value);
 	if (new_node->key == NULL || new_node->value == NULL)
 	{
-		if (new_node->key != NULL)
-			free(new_node->key);
+		free(new_node->key);
+		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
 
-	current_node = ht->array[index];
-	new_node->next = current_node;
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
