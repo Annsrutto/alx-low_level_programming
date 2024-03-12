a52db61b88bde5129830810596f737018d272c1c
Annsrutto <annsrutto567@gmail.com>
2024-03-13 01:54:42 +0300

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,12 +11,10 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
-
 	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	for (size_t i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
